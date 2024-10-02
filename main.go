b6851578d2dae987ae777fa0892145d312d020a0
Trong <spektralfrogadier@gmail.com>
2024-10-01 21:00:20 -0700

    commit to master

--- main.go
+++ main.go
@@ -1,24 +1,67 @@
 package main
 
 import (
-	"fmt"
+  "fmt"
   "strings"
   "math/rand"
+
+// cobra cli
+
+  "github.com/aquilax/truncate"
 )
 
-func getUsr() {
-  name := ""
+func getUsr() string {
+  for {
+    name := ""
 
-  fmt.Println("Enter your username:\n(MAX is 10 characters)")
-  fmt.Scan(&name)
-  username := strings.ToLower(name)
-  fmt.Println("You entered:", username)
+    fmt.Println("Enter your username:\n(MAX is 10 characters)")
+    fmt.Scan(&name)
 
+    if len(name) > 10 {
+      fmt.Println("Username exceeded 10 characters;\nTruncating to the first 10")
+      truncatedName := truncate.Truncate(name, 10, "", truncate.PositionEnd)
+      fmt.Println("Received: ", strings.ToLower(truncatedName), "\nIs this ok?")
 
+      if runCliInput(1) {
+        return strings.ToLower(truncatedName)
+      }
+    } else {
+      fmt.Println("You entered:", strings.ToLower(name), "\nIs this ok?")
+      if runCliInput(1) {
+        return strings.ToLower(name)
+      }
+    }
+  }
+}
+
+/* X - definition & use cases
+1 - Handle Username
+*/
+func runCliInput(x int) bool {
+  for {
+    input := ""
+    fmt.Scan(&input)
+
+    switch strings.ToLower(input) {
+    case "yes", "y":
+      switch x {
+      case 1:
+        return true
+      }
+    case "no", "n":
+      switch x {
+      case 1:
+        return false
+      }
+    default:
+      fmt.Println("Invalid input, please enter 'yes' or 'no'")
+    }
+  }
 }
 
 func main() {
-	fmt.Println("Hello from Nix!")
+  fmt.Println("Hello from Nix!")
   fmt.Println(rand.Intn(3))
-  getUsr()
+  username := getUsr()
+  fmt.Println("Hello", username, "from the Main function")
 }
